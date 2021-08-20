e3117e104d2055eb09a78a8db698fa88735411b9
rahul72925 <rahulkhatri72925@gmail.com>
2021-08-20 19:19:44 +0530

    feat: create order accountancy table in order app

---
+++ admin/src/apps/order/views/Listing/tableOptions.js
@@ -0,0 +1,27 @@
+const options = {
+   cellVertAlign: 'middle',
+   autoResize: true,
+   maxHeight: 420,
+   resizableColumns: false,
+   virtualDomBuffer: 20,
+   placeholder: 'No Data Available',
+   index: 'id',
+   persistence: false,
+   persistenceMode: 'local',
+   selectablePersistence: true,
+   persistence: {
+      group: false,
+      sort: true, //persist column sorting
+      filter: true, //persist filter sorting
+      page: true, //persist page
+      columns: true, //persist columns
+   },
+   layout: 'fitDataStretch',
+   resizableColumns: true,
+   movableColumns: true,
+   tooltips: true,
+   downloadDataFormatter: data => data,
+   downloadReady: (fileContents, blob) => blob,
+}
+
+export default options
