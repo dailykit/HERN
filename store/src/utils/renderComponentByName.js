2477c5c2d483548941b7188b97cfc087f431b82d
Foysal Ahammed <foysal.developer@gmail.com>
2021-08-28 15:54:33 +0600

    feat: page composition on register page

--- store/src/utils/renderComponentByName.js
+++ store/src/utils/renderComponentByName.js
@@ -6,6 +6,12 @@ const Plans = dynamic(() =>
 const Content = dynamic(() =>
    import('../sections/our-menu/content').then(promise => promise.Content)
 )
+const Registration = dynamic(() =>
+   import('../sections/register').then(promise => promise.Registration)
+)
+const StepsNavbar = dynamic(() =>
+   import('../components/steps_navbar').then(promise => promise.StepsNavbar)
+)
 
 export const renderComponentByName = (componentName, options) => {
    const getProps = component => {
@@ -31,6 +37,20 @@ export const renderComponentByName = (componentName, options) => {
       */
       case 'Content':
          return <Content />
+      /*ROUTES : 
+      [brand]/get-started/register
+      */
+      case 'Registration':
+         return <Registration {...getProps('Registration')} />
+      /*ROUTES : 
+      [brand]/get-started/register
+      [brand]/get-started/select-plan
+      [brand]/get-started/select-delivery
+      [brand]/get-started/select-menu
+      [brand]/get-started/checkout
+      */
+      case 'StepsNavbar':
+         return <StepsNavbar />
       default:
          return null
    }
