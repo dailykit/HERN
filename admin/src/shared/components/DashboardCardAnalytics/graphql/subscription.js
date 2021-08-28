e617ab0b9be1c89e87b6e6474b6cb44862cdb774
rahul72925 <rahulkhatri72925@gmail.com>
2021-08-28 16:01:51 +0530

    fix: total revenue card query

--- admin/src/shared/components/DashboardCardAnalytics/graphql/subscription.js
+++ admin/src/shared/components/DashboardCardAnalytics/graphql/subscription.js
@@ -8,6 +8,7 @@ export const GET_TOTAL_EARNING_ORDER_CUSTOMER_TOP_PRODUCT = gql`
          where: {
             isAccepted: { _eq: true }
             cart: { paymentStatus: { _eq: "SUCCEEDED" } }
+            isRejected: { _is_null: true }
          }
       ) {
          aggregate {
