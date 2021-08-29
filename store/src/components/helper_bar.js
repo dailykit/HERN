fd152f6d5d616409d57320e1f66ffc2e4bc32870
Foysal Ahammed <foysal.developer@gmail.com>
2021-08-29 17:14:55 +0600

    feat: added plain css on helper_bar comonent

--- store/src/components/helper_bar.js
+++ store/src/components/helper_bar.js
@@ -1,66 +1,21 @@
-import tw, { styled, css } from 'twin.macro'
+export const HelperBar = ({ type = 'info', children }) => {
+   return <div className={`hern-helper-bar--${type}`}>{children}</div>
+}
 
-export const HelperBar = styled.div(
-   ({ type }) => css`
-      ${tw`py-2 h-auto w-full flex flex-col items-center px-3 rounded mb-3`}
-      ${typePicker(type)}
-   `
-)
-
-const Button = styled.button(
-   () => css`
-      ${tw`mt-1 border py-1 px-2 rounded text-sm`}
-   `
-)
-
-const Title = styled.h4`
-   ${tw`text-center text-lg`}
-`
-
-const SubTitle = styled.h5`
-   ${tw`text-center`}
-`
+const Button = ({ children, onClick }) => {
+   return (
+      <button onClick={onClick} className="hern-helper-bar__btn">
+         {children}
+      </button>
+   )
+}
+const Title = ({ children }) => {
+   return <h4 className="hern-helper-bar__title">{children}</h4>
+}
+const SubTitle = ({ children }) => {
+   return <h5 className="hern-helper-bar__subtitle">{children}</h5>
+}
 
 HelperBar.Button = Button
 HelperBar.Title = Title
 HelperBar.SubTitle = SubTitle
-
-const typePicker = type => {
-   switch (type) {
-      case 'info':
-         return css`
-            ${tw`bg-indigo-200 text-indigo-800`}
-            button {
-               ${tw`border-indigo-800 hover:bg-indigo-300`}
-            }
-         `
-      case 'success':
-         return css`
-            ${tw`bg-green-200 text-green-800`}
-            button {
-               ${tw`border-green-800 hover:bg-green-300`}
-            }
-         `
-      case 'danger':
-         return css`
-            ${tw`bg-red-200 text-red-800`}
-            button {
-               ${tw`border-red-800 hover:bg-red-300`}
-            }
-         `
-      case 'warning':
-         return css`
-            ${tw`bg-orange-200 text-orange-800`}
-            button {
-               ${tw`border-orange-800 hover:bg-orange-300`}
-            }
-         `
-      default:
-         return css`
-            ${tw`bg-indigo-200 text-indigo-800`}
-            button {
-               ${tw`border-indigo-800 hover:bg-indigo-300`}
-            }
-         `
-   }
-}
