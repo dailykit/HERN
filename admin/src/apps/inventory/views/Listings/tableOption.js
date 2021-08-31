df194d1088492388436559b943b0b11becc1a3ef
nitin <inbox.nitin.goyal@gmail.com>
2021-08-31 19:13:10 +0530

    fix: tables styling in inventory app

--- admin/src/apps/inventory/views/Listings/tableOption.js
+++ admin/src/apps/inventory/views/Listings/tableOption.js
@@ -8,8 +8,6 @@ const options = {
    placeholder: 'No Data Available',
    persistence: false,
    persistenceMode: 'cookie',
-   pagination: 'local',
-   paginationSize: 10,
 }
 
 export default options
