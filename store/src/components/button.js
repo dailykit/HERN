39ede3e85ab7327dc49999319a1f417cea393645
Foysal Ahammed <foysal.developer@gmail.com>
2021-09-03 00:02:04 +0600

    feat: added theme color on button by default

--- store/src/components/button.js
+++ store/src/components/button.js
@@ -1,15 +1,15 @@
 import classNames from 'classnames'
+import { useThemeStyle } from '../utils'
 
 export const Button = ({ children, disabled, bg, onClick, className }) => {
+   const themeColor = useThemeStyle('backgroundColor')
    const buttonClasses = classNames('hern__btn', {
       'hern__btn--disabled': disabled,
    })
    return (
       <button
          onClick={onClick}
-         style={{
-            backgroundColor: bg ? bg : 'rgba(5, 150, 105, 1)',
-         }}
+         style={themeColor}
          className={`${buttonClasses} ${className ? className : ''}`}
          disabled={disabled}
       >
