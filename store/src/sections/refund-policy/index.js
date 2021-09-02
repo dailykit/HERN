f8805e1d1a58e706deaaaea4cbf8b13ad6de2d36
Foysal Ahammed <foysal.developer@gmail.com>
2021-09-02 19:17:14 +0600

    feat: added plain css on refund policy

--- store/src/sections/refund-policy/index.js
+++ store/src/sections/refund-policy/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import tw from 'twin.macro'
 import ReactHtmlParser from 'react-html-parser'
-import { StyledArticle } from '../../components'
 import { useConfig } from '../../lib'
 
 export const RefundPolicy = () => {
    const { value } = useConfig('brand').configOf('Refund Policy')
    return (
-      <div tw="min-h-full text-gray-600 md:mx-64 mx-10 mb-4">
-         <h1 tw="my-10  text-5xl text-gray-800 text-center py-2 border-gray-200 border-b-2">
-            Refund Policy
-         </h1>
-         <div tw="text-lg">
-            <StyledArticle>{ReactHtmlParser(value)}</StyledArticle>
+      <div className="hern-refund-policy">
+         <h1 className="hern-refund-policy__heading">Refund Policy</h1>
+         <div className="hern-refund-policy__content">
+            <article className="hern-refund-policy__article">
+               {ReactHtmlParser(value)}
+            </article>
          </div>
       </div>
    )
