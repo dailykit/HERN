6a00e59f77619f4109dca75324fafa546731864b
Deepak Negi <st.deepak15@gmail.com>
2021-09-07 10:42:44 +0530

    feat: verifying payment integration type at stripe root wrapper

--- server/lib/stripe.js
+++ server/lib/stripe.js
@@ -1,10 +1,15 @@
 import Stripe from 'stripe'
-
+import { isConnectedIntegration } from '../utils'
 import get_env from '../../get_env'
 
 const stripe = async () => {
    const STRIPE_SECRET_KEY = await get_env('STRIPE_SECRET_KEY')
-   return new Stripe(STRIPE_SECRET_KEY)
+   const STRIPE_ACCOUNT_ID = await get_env('STRIPE_ACCOUNT_ID')
+   return new Stripe(STRIPE_SECRET_KEY, {
+      ...((await isConnectedIntegration()) && {
+         stripeAccount: STRIPE_ACCOUNT_ID
+      })
+   })
 }
 
 export default stripe
