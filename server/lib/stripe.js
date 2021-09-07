8b2df3a1c3792f2d7ed4bc6d365dbdcd15a3d2aa
Deepak Negi <st.deepak15@gmail.com>
2021-09-07 16:50:30 +0530

    refactor: stripe lib function

--- server/lib/stripe.js
+++ server/lib/stripe.js
@@ -5,8 +5,9 @@ import get_env from '../../get_env'
 const stripe = async () => {
    const STRIPE_SECRET_KEY = await get_env('STRIPE_SECRET_KEY')
    const STRIPE_ACCOUNT_ID = await get_env('STRIPE_ACCOUNT_ID')
+   const isConnected = await isConnectedIntegration()
    return new Stripe(STRIPE_SECRET_KEY, {
-      ...((await isConnectedIntegration()) && {
+      ...(isConnected && {
          stripeAccount: STRIPE_ACCOUNT_ID
       })
    })
