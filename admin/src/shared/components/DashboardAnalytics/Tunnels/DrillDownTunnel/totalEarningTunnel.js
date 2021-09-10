65c0f4123707e7ee9878cc6bf082f0eb324b589d
rahul72925 <rahulkhatri72925@gmail.com>
2021-09-10 19:07:53 +0530

    fix: tooltip in total earning graph

--- admin/src/shared/components/DashboardAnalytics/Tunnels/DrillDownTunnel/totalEarningTunnel.js
+++ admin/src/shared/components/DashboardAnalytics/Tunnels/DrillDownTunnel/totalEarningTunnel.js
@@ -614,6 +614,7 @@ const DrillDownLineChart = ({
                            groupBy={groupBy[groupBy.length - 1]}
                            dataOf={dataOf}
                            currency={currency}
+                           isCompareSkip={compare.isSkip}
                         />
                      }
                   />
@@ -622,7 +623,7 @@ const DrillDownLineChart = ({
                      type="monotone"
                      name="Earning"
                      dataKey={dataOf}
-                     stroke="#8884d8"
+                     stroke="#2AC981"
                      strokeWidth={2}
                      activeDot={{
                         onClick: (event, payload) => {
@@ -651,7 +652,7 @@ const DrillDownLineChart = ({
                         type="monotone"
                         name="Compare Earning"
                         dataKey={dataOf + 'Compare'}
-                        stroke="#C9D8B6"
+                        stroke="#8884d8"
                         activeDot={{ r: 4 }}
                         strokeWidth={2}
                      />
@@ -669,8 +670,29 @@ const TunnelBody = styled.div`
    height: calc(100% - 103px);
    overflow: auto;
 `
-const CustomTooltip = ({ active, payload, dataOf, currency }) => {
+const CustomTooltip = ({
+   active,
+   payload,
+   dataOf,
+   currency,
+   isCompareSkip,
+   groupBy,
+}) => {
    if (active && payload && payload.length) {
+      const date = payload[0].payload['present']
+      let timeToBeShow = ''
+      if (groupBy == 'hour') {
+         timeToBeShow = moment(date).format('LT')
+      } else if (groupBy == 'day') {
+         timeToBeShow = moment(date).format('DD-MMM')
+      } else if (groupBy == 'week') {
+         timeToBeShow =
+            moment(date).format('DD-MMM-YY') +
+            ' to ' +
+            moment(date).add(1, 'week').format('DD-MMM-YY')
+      } else {
+         timeToBeShow = moment(date).format('MMM-YYYY')
+      }
       return (
          <div
             style={{
@@ -685,17 +707,46 @@ const CustomTooltip = ({ active, payload, dataOf, currency }) => {
             }}
          >
             <Spacer size="3px" />
-            <Text as="text3">{`${dataOf.toUpperCase()}: ${currency}${
-               payload[0].payload[dataOf]
-            }`}</Text>
-            <Text as="text3">
-               {`onDemand : ${currency}${payload[0].payload['onDemand'] || 0}`}
-            </Text>
-            <Text as="text3">
-               {`Subscription : ${currency}${
-                  payload[0].payload['subscription'] || 0
-               }`}
-            </Text>
+            <Text as="text3">{timeToBeShow}</Text>
+            <Flex>
+               {`${dataOf.toUpperCase()}: `}{' '}
+               <span style={{ color: '#2AC981' }}>
+                  {`${currency}${payload[0].payload[dataOf]} `}
+               </span>
+               {!isCompareSkip ? (
+                  <span style={{ color: '#8884d8' }}>
+                     {`${currency}${
+                        payload[0].payload[dataOf + 'Compare'] || 0
+                     }`}
+                  </span>
+               ) : null}
+            </Flex>
+            <Flex>
+               {`onDemand`}{' '}
+               <span style={{ color: '#2AC981' }}>
+                  {`${currency}${payload[0].payload['onDemand'] || 0} `}
+               </span>
+               {!isCompareSkip ? (
+                  <span style={{ color: '#8884d8' }}>
+                     {`${currency}${
+                        payload[0].payload['onDemand' + 'Compare'] || 0
+                     }`}
+                  </span>
+               ) : null}
+            </Flex>
+            <Flex>
+               {`Subscription`}{' '}
+               <span style={{ color: '#2AC981' }}>
+                  {`${currency}${payload[0].payload['subscription'] || 0} `}
+               </span>
+               {!isCompareSkip ? (
+                  <span style={{ color: '#8884d8' }}>
+                     {`${currency}${
+                        payload[0].payload['subscription' + 'Compare'] || 0
+                     }`}
+                  </span>
+               ) : null}
+            </Flex>
          </div>
       )
    }
