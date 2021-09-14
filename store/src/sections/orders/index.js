11cc34f70632601b76205f5765c9416807773208
Foysal Ahammed <foysal.developer@gmail.com>
2021-09-14 23:57:39 +0600

    feat: added plain css on order section

--- store/src/sections/orders/index.js
+++ store/src/sections/orders/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import tw, { styled, css } from 'twin.macro'
 import { useSubscription } from '@apollo/react-hooks'
 
 import { useConfig } from '../../lib'
@@ -10,21 +9,22 @@ import { ORDER_HISTORY, ORDER } from '../../graphql'
 import { HelperBar, ProfileSidebar, ProductSkeleton } from '../../components'
 import OrderInfo from '../../sections/OrderInfo'
 import { route } from 'next/dist/next-server/server/router'
+import classNames from 'classnames'
 
 export const Orders = () => {
    return (
-      <Main>
+      <main className="hern-orders__main">
          <ProfileSidebar />
          <OrderHistory />
-      </Main>
+      </main>
    )
 }
 const OrderHistory = () => {
    return (
-      <Wrapper>
+      <div className="hern-orders__order-history">
          <Listing />
          <Details />
-      </Wrapper>
+      </div>
    )
 }
 
@@ -60,46 +60,53 @@ const Listing = () => {
       router.push(getRoute(`/account/orders?id=${id}`))
    }
 
-   if (loading)
-      return (
-         <aside tw="border-r overflow-y-auto">
-            <h2 tw="px-3 pt-3 pb-1 mb-2 text-green-600 text-2xl">Orders</h2>
-            <ul tw="px-2 space-y-2">
-               <li tw="bg-gray-300 px-2 h-10 rounded"></li>
-               <li tw="bg-gray-200 px-2 h-10 rounded"></li>
-               <li tw="bg-gray-100 px-2 h-10 rounded"></li>
-            </ul>
-         </aside>
-      )
+   if (loading) return <OrderListSkeleton />
    return (
-      <aside tw="border-r overflow-y-auto">
-         <Title theme={theme}>Orders</Title>
-         <ul tw="px-2 space-y-2">
+      <aside className="hern-orders__order-listing">
+         <h2
+            className="hern-orders__order-listing__heading"
+            style={{
+               color: `${theme.accent ? theme.accent : 'rgba(5,150,105,1)'}`,
+            }}
+            theme={theme}
+         >
+            Orders
+         </h2>
+         <ul className="hern-orders__order-listing__list">
             {orders.nodes.map(
                (node, i) =>
                   (i + 1 <= orderWindow ||
                      (isClient && window.innerWidth > 786)) && (
-                     <Date
-                        theme={theme}
+                     <li
                         key={node.occurrenceId}
                         onClick={() => selectOrder(node.occurenceId)}
-                        className={`${
-                           node.occurenceId === Number(router.query.id)
-                              ? 'active'
-                              : ''
-                        }`}
+                        style={{
+                           backgroundColor: `${
+                              theme.highlight &&
+                              node.occurenceId === Number(router.query.id)
+                                 ? theme.highlight
+                                 : '#fff'
+                           }`,
+                        }}
+                        className={classNames(
+                           'hern-orders__order-listing__list-item',
+                           {
+                              'hern-orders__order-listing__list-item--active':
+                                 node.occurenceId === Number(router.query.id),
+                           }
+                        )}
                      >
                         {formatDate(node.occurence.date, {
                            month: 'short',
                            day: 'numeric',
                            year: 'numeric',
                         })}
-                     </Date>
+                     </li>
                   )
             )}
             {orders.nodes.length > orderWindow && (
                <div
-                  tw="float-right text-sm text-blue-500 block md:hidden"
+                  className="hern-orders__order-listing__view-more"
                   onClick={() => setOrderWindow(orderWindow + 4)}
                >
                   View More
@@ -135,19 +142,11 @@ const Details = () => {
    )
    const theme = configOf('theme-color', 'Visual')
 
-   if (loading)
-      return (
-         <main tw="mx-3">
-            <h2 tw="pt-3 pb-1 mb-2 text-green-600 text-2xl">Order Details</h2>
-            <ProductCards>
-               <ProductSkeleton />
-               <ProductSkeleton />
-            </ProductCards>
-         </main>
-      )
+   if (loading) return <OrderDetailsSkeleton />
+
    if (!router.query.id)
       return (
-         <div tw="m-3">
+         <div className="hern-orders__order-details">
             <HelperBar type="warning">
                <HelperBar.SubTitle>
                   Select a date to view an order details
@@ -156,38 +155,58 @@ const Details = () => {
          </div>
       )
    return (
-      <main tw="mx-3">
-         <header tw="flex items-center justify-between">
-            <Title theme={theme}>Order Details</Title>
+      <main className="hern-orders__order-details">
+         <header className="hern-orders__order-details__header">
+            <h2
+               className="hern-orders__order-details__header__title"
+               style={{
+                  color: `${
+                     theme?.accent ? theme?.accent : 'rgba(5,150,105,1)'
+                  }`,
+               }}
+            >
+               Order Details
+            </h2>
             {order?.cart?.orderStatus?.title && (
-               <Status status={order?.cart?.orderStatus?.title}>
+               <span
+                  className="hern-orders__order-details__status"
+                  style={{
+                     backgroundColor: `${selectColor(
+                        order?.cart?.orderStatus?.title
+                     )}`,
+                  }}
+               >
                   {order?.cart?.orderStatus?.title}
-               </Status>
+               </span>
             )}
          </header>
          <OrderInfo cart={order?.cart} />
-         <h4 tw="text-lg text-gray-700 my-4 pb-1 border-b">Payment</h4>
+         <h4 className="hern-orders__order-details__payment__heading">
+            Payment
+         </h4>
          {order?.cart?.paymentStatus !== 'SUCCEEDED' && (
             <button
+               className="hern-orders__order-details__payment__complete-payment"
                onClick={() => route.push(`/checkout?id=${order?.cart?.id}`)}
-               tw="rounded py-2 bg-red-500 text-white px-6 uppercase tracking-wider mb-3"
             >
                Complete Payment
             </button>
          )}
-         <section tw="mb-3 p-2 border w-full">
+         <section className="hern-orders__order-details__payment-method">
             {paymentMethod ? (
                <>
-                  <div tw="rounded flex items-center justify-between">
-                     <span tw="text-xl">{paymentMethod?.cardHolderName}</span>
-                     <div tw="flex items-center">
-                        <span tw="font-medium">{paymentMethod?.expMonth}</span>
+                  <div className="hern-orders__order-details__payment__card">
+                     <span className="hern-orders__order-details__payment__card__name">
+                        {paymentMethod?.cardHolderName}
+                     </span>
+                     <div>
+                        <span>{paymentMethod?.expMonth}</span>
                         &nbsp;/&nbsp;
-                        <span tw="font-medium">{paymentMethod?.expYear}</span>
+                        <span>{paymentMethod?.expYear}</span>
                      </div>
                   </div>
                   <span>
-                     <span tw="text-gray-500">Last 4:</span>{' '}
+                     <span>Last 4:</span>
                      {paymentMethod?.last4}
                   </span>
                </>
@@ -198,58 +217,26 @@ const Details = () => {
       </main>
    )
 }
-
-const Main = styled.main`
-   display: grid;
-   grid-template-rows: 1fr;
-   grid-template-columns: 240px 1fr;
-   @media (max-width: 768px) {
-      display: block;
-   }
-`
-
-const ProductCards = styled.ul`
-   display: grid;
-   grid-gap: 16px;
-   grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
-`
-
-const Title = styled.h2(
-   ({ theme }) => css`
-      ${tw`px-3 pt-3 pb-1 mb-2 text-green-600 text-2xl`}
-      ${theme?.accent && `color: ${theme.accent}`}
-   `
-)
-
-const Wrapper = styled.div`
-   display: grid;
-   background: #fff;
-   grid-template-columns: 280px 1fr;
-   > aside {
-      height: calc(100vh - 64px);
-   }
-   @media (max-width: 768px) {
-      display: block;
-      > aside {
-         height: max-content;
-      }
-   }
-`
-
-const Date = styled.li(
-   ({ theme }) => css`
-      ${tw`cursor-pointer px-2 py-2 rounded hover:(text-white bg-green-400)`}
-      &.active {
-         ${tw`text-white bg-green-600 hover:(bg-green-700)`}
-         ${theme?.highlight &&
-         css`
-            background: ${theme.highlight};
-            :hover {
-               background: ${theme.highlight};
-            }
-         `}
-      }
-   `
+const OrderListSkeleton = () => {
+   return (
+      <aside className="hern-orders__list__skeleton">
+         <h2>Orders</h2>
+         <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+         </ul>
+      </aside>
+   )
+}
+const OrderDetailsSkeleton = () => (
+   <main classNames="hern-orders__order-details__skeleton">
+      <h2>Order Details</h2>
+      <ProductCards>
+         <ProductSkeleton />
+         <ProductSkeleton />
+      </ProductCards>
+   </main>
 )
 
 const selectColor = variant => {
@@ -268,10 +255,3 @@ const selectColor = variant => {
          return '#FF5A52'
    }
 }
-
-const Status = styled.span(
-   ({ status }) => css`
-      background: ${selectColor(status)};
-      ${tw`px-2 py-1 rounded text-white`}
-   `
-)
