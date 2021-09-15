a57e1be3d8db98c81f8a34de35897a220deea520
Deepak Negi <st.deepak15@gmail.com>
2021-09-15 21:10:21 +0530

    fix: add brad-alias only for localhost:3000

--- store/src/utils/getRoute.js
+++ store/src/utils/getRoute.js
@@ -1,5 +1,8 @@
+import { isClient } from './isClient'
+
 export const getRoute = route => {
-   if (process.env.NODE_ENV === 'development') {
+   const host = isClient ? window.location.host : ''
+   if (process.env.NODE_ENV === 'development' && host === 'localhost:3000') {
       return `/test${route}`
    }
    return route
