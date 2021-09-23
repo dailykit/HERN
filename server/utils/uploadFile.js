5ea14021c4648f9d80a154999c0eb304b6a2eb22
Deepak Negi <st.deepak15@gmail.com>
2021-09-23 16:24:11 +0530

    feat: added image compression on image upload

--- server/utils/uploadFile.js
+++ server/utils/uploadFile.js
@@ -1,14 +1,18 @@
-import get_env from '../../get_env'
-
 import aws from '../lib/aws'
+import get_env from '../../get_env'
+import { minifyImage } from '../utils'
 
 export const uploadFile = async (buffer, name, type) => {
    const AWS = await aws()
    const s3 = new AWS.S3()
    const S3_BUCKET = await get_env('S3_BUCKET')
+
+   // minify image buffer
+   const minifiedBuffer = await minifyImage(buffer)
+
    const params = {
       ACL: 'public-read',
-      Body: buffer,
+      Body: minifiedBuffer,
       Bucket: S3_BUCKET,
       ContentType: type.mime,
       Key: `${name}.${type.ext}`,
