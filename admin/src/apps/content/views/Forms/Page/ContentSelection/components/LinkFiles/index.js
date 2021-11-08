67ea2b05a8e939ea2cc241d52092066ed9b1dab5
Foysal Ahammed <foysal.developer@gmail.com>
2021-11-08 21:47:53 +0600

    feat: added link CSS and JS adding functionality

---
+++ admin/src/apps/content/views/Forms/Page/ContentSelection/components/LinkFiles/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, Flex } from '@dailykit/ui'
+import Panel from './Panel'
+
+const LinkFiles = () => (
+   <div>
+      <Flex padding="16px 8px">
+         <Text as="text1">Page Settings</Text>
+      </Flex>
+      <Panel />
+   </div>
+)
+export default LinkFiles
