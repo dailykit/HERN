5ae89a927c941f47cd3a5c611fd698c761e32228
rupalmalik3 <rupalmalik3@gmail.com>
2021-11-10 12:44:39 +0530

    remove: section folder inside BrandSettings

--- admin/src/apps/brands/views/Forms/brand/tabs/BrandSettings/sections/Coupons/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { SettingsCard } from '../../SettingsCard'
-
-export const Coupons = () => {
-   return <SettingsCard identifier="Coupons Availability" type="rewards"  title="Rewards"/>
-}
