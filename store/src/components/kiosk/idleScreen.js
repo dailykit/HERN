72ec0e14407570ff51666ed3c76bc73986b91bc1
Foysal Ahammed <foysal.developer@gmail.com>
2021-11-24 11:50:44 +0600

    feat: added video on idle page

--- store/src/components/kiosk/idleScreen.js
+++ store/src/components/kiosk/idleScreen.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import Image from 'next/image'
+import ReactPlayer from 'react-player'
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
 
 export const IdleScreen = props => {
    const { config } = props
+   const [playing, setPlaying] = React.useState(0)
    const properties = {
       duration: 3000,
       autoplay: true,
@@ -17,22 +18,62 @@ export const IdleScreen = props => {
       const b = document.querySelector('body')
       b.style.padding = 0
    }, [])
+
+   const videoURLs = config.idlePageSettings.idlePageVideo.value.assets.urls
+   const playNextFile = () => {
+      if (playing === videoURLs.length - 1) {
+         setPlaying(0)
+      } else {
+         setPlaying(playing + 1)
+      }
+   }
+
    return (
       <div className="hern-kiosk__idle-screen-container">
-         <Slide {...properties} cssClass="hern-kiosk__idle-screen-image-slider">
-            {config.idlePageSettings.idlePageImage.value.assets.images.map(
-               (image, index) => {
-                  return (
-                     <img
-                        src={image}
-                        key={index}
-                        alt="Picture of the author"
-                        className="hern-kiosk__idle-screen-image"
-                     />
-                  )
-               }
-            )}
-         </Slide>
+         {config.idlePageSettings.idlePageVideo &&
+         ReactPlayer.canPlay(videoURLs[playing]) ? (
+            <>
+               {/*TODO: Data types and useIsertType should be changed on the JSON file */}
+               {config.idlePageSettings.idlePageVideo.value.assets
+                  .backgroundImage && (
+                  <div
+                     style={{
+                        backgroundImage: `url('${config.idlePageSettings.idlePageVideo.value.assets.backgroundImage}')`,
+                     }}
+                     className="hern-kiosk__idle-screen-video__background"
+                  ></div>
+               )}
+
+               <ReactPlayer
+                  muted
+                  className="hern-kiosk__idle-screen-video"
+                  playing={true}
+                  controls={false}
+                  height="100vh"
+                  width="100vw"
+                  url={videoURLs[playing]}
+                  onEnded={playNextFile}
+               />
+            </>
+         ) : (
+            <Slide
+               {...properties}
+               cssClass="hern-kiosk__idle-screen-image-slider"
+            >
+               {config.idlePageSettings.idlePageImage.value.assets.images.map(
+                  (image, index) => {
+                     return (
+                        <img
+                           src={image}
+                           key={index}
+                           alt="Picture of the author"
+                           className="hern-kiosk__idle-screen-image"
+                        />
+                     )
+                  }
+               )}
+            </Slide>
+         )}
          <footer
             style={{
                backgroundColor: `${config.kioskSettings.theme.primaryColor.value}`,
