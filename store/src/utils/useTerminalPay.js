7a84b7d673252f10ddd02e7e9e1409490f0f08f1
Deepak Negi <st.deepak15@gmail.com>
2021-12-22 17:59:06 +0530

    refactor:useTerminalPay client-side util function

--- store/src/utils/useTerminalPay.js
+++ store/src/utils/useTerminalPay.js
@@ -1,26 +1,50 @@
 import React from 'react'
-import { isClient } from '../utils'
+import { useMutation } from '@apollo/react-hooks'
 import axios from 'axios'
+import isEmpty from 'lodash/isEmpty'
+
+import { isClient } from '../utils'
+import { UPDATE_CART, UPDATE_CART_PAYMENT } from '../graphql'
+
+const passResponseToWebhook = async data => {
+   const baseUrl = isClient ? window.location.origin : ''
+   const url = `${baseUrl}/server/api/payment/handle-payment-webhook`
+   const response = await axios({
+      url,
+      method: 'POST',
+      headers: {
+         'payment-type': 'terminal',
+      },
+      data,
+   })
+   return response.data
+}
 
 function useTerminalPay() {
+   const [updateCart] = useMutation(UPDATE_CART, {
+      onError: error => {
+         console.error(error)
+      },
+   })
+   const [updateCartPayment] = useMutation(UPDATE_CART_PAYMENT, {
+      onError: error => {
+         console.error(error)
+      },
+   })
+
+   // React.useEffect(() => {
+   //    // here we listen the responses coming from terminal
+   //    // and pass them to the webhook endpoint
+   // },[])
+
    const initiateTerminalPayment = async cartPayment => {
       console.log('initiateTerminalPayment')
       const terminalResponseData = {
          cartPaymentId: cartPayment.id,
-         status: 'PROCESSING',
+         status: 'SWIPE_OR_INSERT',
          transactionId: `TXN-${new Date().getTime()}`,
       }
-      const baseUrl = isClient ? window.location.origin : ''
-      const url = `${baseUrl}/server/api/payment/handle-payment-webhook`
-      const response = await axios({
-         url,
-         method: 'POST',
-         headers: {
-            'payment-type': 'terminal',
-         },
-         data: terminalResponseData,
-      })
-      return response.data
+      return await passResponseToWebhook(terminalResponseData)
    }
 
    const byPassTerminalPayment = async ({ type, cartPayment }) => {
@@ -28,7 +52,7 @@ function useTerminalPay() {
 
       const terminalResponseData = {
          cartPaymentId: cartPayment.id,
-         status: type === 'success' ? 'SUCCEEDED' : 'FAILED',
+         status: type === 'success' ? 'succeeded' : 'payment_failed',
          transactionId: `TXN-${new Date().getTime()}`,
          transactionRemark: {
             id: 'NA',
@@ -37,19 +61,48 @@ function useTerminalPay() {
             reason: 'test mode',
          },
       }
-      const baseUrl = isClient ? window.location.origin : ''
-      const url = `${baseUrl}/server/api/payment/handle-payment-webhook`
-      const response = await axios({
-         url,
-         method: 'POST',
-         data: terminalResponseData,
+      return await passResponseToWebhook(terminalResponseData)
+   }
+
+   const cancelTerminalPayment = async ({
+      codPaymentOptionId,
+      cartPayment,
+   }) => {
+      console.log('cancelling terminal payment')
+      const terminalResponseData = {
+         cartPaymentId: cartPayment.id,
+         status: 'cancelled',
+         transactionId: `TXN-${new Date().getTime()}`,
+      }
+      const { data } = await updateCartPayment({
+         variables: {
+            id: cartPayment.id,
+            _set: {
+               paymentStatus: 'CANCELLED',
+               isResultShown: true,
+            },
+         },
       })
-      return response.data
+      if (
+         !isEmpty(data) &&
+         data.updateCartPayment.paymentStatus === 'CANCELLED'
+      ) {
+         await updateCart({
+            variables: {
+               id: data.updateCartPayment.cartId,
+               _inc: { paymentRetryAttempt: 1 },
+               _set: {
+                  toUseAvailablePaymentOptionId: codPaymentOptionId,
+               },
+            },
+         })
+      }
    }
 
    return {
       initiateTerminalPayment,
       byPassTerminalPayment,
+      cancelTerminalPayment,
    }
 }
 
