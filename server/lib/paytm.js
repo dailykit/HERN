4ff8f4795490d31aab10b7233b34ebe2d09b078e
Deepak Negi <st.deepak15@gmail.com>
2022-01-03 12:39:56 +0530

    fix: dynamic callbackUrl for paytm payment

--- server/lib/paytm.js
+++ server/lib/paytm.js
@@ -2,7 +2,7 @@ import get_env from '../../get_env'
 
 const Paytm = require('paytm-pg-node-sdk')
 
-const paytm = async () => {
+const paytm = async host => {
    const PAYTM_ENVIRONMENT = Paytm.LibraryConstants.STAGING_ENVIRONMENT
    const PAYTM_MERCHANT_ID =
       (await get_env('PAYTM_MERCHANT_ID')) || process.env.PAYTM_MERCHANT_ID
@@ -10,8 +10,7 @@ const paytm = async () => {
       (await get_env('PAYTM_MERCHANT_KEY')) || process.env.PAYTM_MERCHANT_KEY
    const PAYTM_WEBSITE =
       (await get_env('PAYTM_WEBSITE')) || process.env.PAYTM_WEBSITE
-   const PAYTM_CALLBACK_URL =
-      'https://dailyos-backend.ngrok.io/server/api/payment/handle-payment-webhook'
+   const PAYTM_CALLBACK_URL = `${host}/server/api/payment/handle-payment-webhook`
    Paytm.MerchantProperties.setCallbackUrl(PAYTM_CALLBACK_URL)
    Paytm.MerchantProperties.initialize(
       PAYTM_ENVIRONMENT,
