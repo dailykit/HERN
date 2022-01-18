3b95bcc53a1e66cda0aa28df888aec51959b58f7
nitin <inbox.nitin.goyal@gmail.com>
2022-01-18 14:36:41 +0530

    feat: add click funtionality to weekly analysis cards on dashboard

--- admin/src/shared/components/dashboardWeeklyAnalysis/index.js
+++ admin/src/shared/components/dashboardWeeklyAnalysis/index.js
@@ -1,19 +1,43 @@
+import { Tunnel, TunnelHeader, Tunnels, useTunnel } from '@dailykit/ui'
 import React, { useState } from 'react'
+import EarningOverTime from '../Reports/ReportTiles/TotalEarnings/Tunnels/earningOverTime'
+import OrderSummaryReport from '../Reports/ReportTiles/TotalOrders/tunnels/orderSummary'
+
 import * as Styles from './styled'
 import WeeklyEarning from './WeeklyEarning'
 import WeeklyOrderReceive from './WeeklyOrderReceive'
 import WeeklySubscribers from './WeeklySubscribers'
 
 const DashboardWeeklyAnalysis = () => {
+   const [tunnels, openTunnel, closeTunnel] = useTunnel(2)
+
    return (
       <>
          <Styles.WeeklyReportContainer>
             <Styles.WeeklyReports>
-               <WeeklyEarning />
-               <WeeklyOrderReceive />
+               <WeeklyEarning onClick={() => openTunnel(1)} />
+               <WeeklyOrderReceive onClick={() => openTunnel(2)} />
                <WeeklySubscribers />
             </Styles.WeeklyReports>
          </Styles.WeeklyReportContainer>
+         <Tunnels tunnels={tunnels}>
+            <Tunnel size="full" layer={1}>
+               <TunnelHeader
+                  title="Earning over time"
+                  close={() => closeTunnel(1)}
+                  description="This is a description"
+               />
+               <EarningOverTime />
+            </Tunnel>
+            <Tunnel size="full" layer={2}>
+               <TunnelHeader
+                  title="Order Summary Report"
+                  close={() => closeTunnel(2)}
+                  description="This is a description"
+               />
+               <OrderSummaryReport />
+            </Tunnel>
+         </Tunnels>
       </>
    )
 }
