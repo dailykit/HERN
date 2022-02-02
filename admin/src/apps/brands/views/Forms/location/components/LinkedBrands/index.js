c37c6a1323e461ccbee4e53820518be0d9781229
nitin <inbox.nitin.goyal@gmail.com>
2022-02-02 19:15:38 +0530

    feat: toggle for service type in location brand card

--- admin/src/apps/brands/views/Forms/location/components/LinkedBrands/index.js
+++ admin/src/apps/brands/views/Forms/location/components/LinkedBrands/index.js
@@ -1,4 +1,4 @@
-import { useSubscription } from '@apollo/react-hooks'
+import { useMutation, useSubscription } from '@apollo/react-hooks'
 import {
    ButtonGroup,
    ButtonTile,
@@ -9,13 +9,28 @@ import {
    Tunnels,
    useTunnel,
 } from '@dailykit/ui'
+import { Switch } from 'antd'
 import React from 'react'
+import { toast } from 'react-toastify'
 import { PlusIcon } from '../../../../../../../shared/assets/icons'
 import { InlineLoader } from '../../../../../../../shared/components'
-import { Container } from '../../../../../../../shared/components/Nutrition/styled'
+import { logger } from '../../../../../../../shared/utils'
+import {
+   BottomBrandLocationCard,
+   DeliveryIcon,
+   DineInIcon,
+   PickUpIcon,
+   TopBrandLocationCard,
+} from '../../../../../assets/illustration'
 import { BRAND_LOCATION } from '../../../../../graphql'
 import { LinkedBrandsTunnel } from '../../tunnels'
-import { StyledHeader } from './styled'
+import {
+   StyledCard,
+   StyledContent,
+   StyledHeader,
+   StyledServiceToggle,
+   StyledServiceType,
+} from './styled'
 
 export const LinkedBrands = ({ state, locationId }) => {
    const [tunnels, openTunnel, closeTunnel] = useTunnel(1)
@@ -34,6 +49,30 @@ export const LinkedBrands = ({ state, locationId }) => {
          setLinkedBrands(data.subscriptionData.data.brands_brand_location)
       },
    })
+
+   //mutation
+   const [updateBrandLocation] = useMutation(BRAND_LOCATION.UPDATE, {
+      onCompleted: () => {
+         toast.success('Updated!')
+      },
+      onError: error => {
+         toast.error('Something went wrong!')
+         // console.log('error', error)
+         logger(error)
+      },
+   })
+
+   //handler
+   const handleToggleUpdate = ({ field, brandId, value }) =>
+      updateBrandLocation({
+         variables: {
+            brandId: { _eq: brandId },
+            locationId: { _eq: locationId },
+            _set: {
+               [field]: !value,
+            },
+         },
+      })
 
    return (
       <>
@@ -63,6 +102,99 @@ export const LinkedBrands = ({ state, locationId }) => {
                         </ComboButton>
                      </ButtonGroup>
                   </StyledHeader>
+                  {LinkedBrands.map(eachBrand => (
+                     <>
+                        <StyledCard
+                           key={`${eachBrand.brandId}-${eachBrand.locationId}`}
+                        >
+                           <TopBrandLocationCard />
+                           <StyledContent>
+                              <Flex>
+                                 <Text as="h3">{eachBrand.brand.title}</Text>
+                              </Flex>
+                              <StyledServiceType>
+                                 <StyledServiceToggle>
+                                    <DeliveryIcon />
+                                    <Switch
+                                       name={`brand-${eachBrand.brandId}`}
+                                       value={eachBrand.doesDeliver}
+                                       checked={eachBrand.doesDeliver}
+                                       checkedChildren="Deliver"
+                                       unCheckedChildren="Deliver"
+                                       title="Press to change Delivery Service"
+                                       onChange={() =>
+                                          updateBrandLocation({
+                                             variables: {
+                                                brandId: eachBrand.brandId,
+
+                                                locationId:
+                                                   eachBrand.locationId,
+                                                _set: {
+                                                   doesDeliver:
+                                                      !eachBrand.doesDeliver,
+                                                },
+                                             },
+                                          })
+                                       }
+                                    />
+                                 </StyledServiceToggle>
+                                 <StyledServiceToggle>
+                                    <PickUpIcon />
+                                    <Switch
+                                       name={`brand-${eachBrand.brandId}`}
+                                       value={eachBrand.doesPickup}
+                                       checked={eachBrand.doesPickup}
+                                       checkedChildren="Pickup"
+                                       unCheckedChildren="Pickup"
+                                       title="Press to change Pickup Service"
+                                       onChange={() =>
+                                          updateBrandLocation({
+                                             variables: {
+                                                brandId: eachBrand.brandId,
+
+                                                locationId:
+                                                   eachBrand.locationId,
+                                                _set: {
+                                                   doesPickup:
+                                                      !eachBrand.doesPickup,
+                                                },
+                                             },
+                                          })
+                                       }
+                                    />
+                                 </StyledServiceToggle>
+                                 <StyledServiceToggle>
+                                    <DineInIcon />
+                                    <Switch
+                                       name={`brand-${eachBrand.brandId}`}
+                                       value={eachBrand.doesDinein}
+                                       checked={eachBrand.doesDinein}
+                                       checkedChildren="Dinein"
+                                       unCheckedChildren="Dinein"
+                                       title="Press to change Dinein Service"
+                                       onChange={() =>
+                                          updateBrandLocation({
+                                             variables: {
+                                                brandId: eachBrand.brandId,
+
+                                                locationId:
+                                                   eachBrand.locationId,
+                                                _set: {
+                                                   doesDinein:
+                                                      !eachBrand.doesDinein,
+                                                },
+                                             },
+                                          })
+                                       }
+                                    />
+                                 </StyledServiceToggle>
+                              </StyledServiceType>
+                              <Flex></Flex>
+                           </StyledContent>
+                           <BottomBrandLocationCard />
+                        </StyledCard>
+                     </>
+                  ))}
                </Flex>
             </>
          ) : (
--- admin/src/apps/brands/views/Forms/location/components/LinkedBrands/styled.js
+++ admin/src/apps/brands/views/Forms/location/components/LinkedBrands/styled.js
@@ -5,3 +5,25 @@ export const StyledHeader = styled.div`
    align-items: center;
    justify-content: space-between;
 `
+export const StyledCard = styled.div`
+   display: flex;
+   flex-direction: column;
+`
+export const StyledContent = styled.div`
+   display: flex;
+   width: 17rem;
+   height: auto;
+   flex-direction: column;
+   padding: 0.5rem;
+`
+export const StyledServiceType = styled.div`
+   display: grid;
+   grid-template-columns: auto auto auto;
+   justify-items: center;
+`
+export const StyledServiceToggle = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 0.5em;
+`
