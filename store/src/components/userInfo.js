ad4117e8104c14a1071b8c195110bbe665fe5279
FASami <foysal.developer@gmail.com>
2022-02-02 21:07:12 +0600

    tweak: user info inside tunnel on smaller screen

--- store/src/components/userInfo.js
+++ store/src/components/userInfo.js
@@ -5,6 +5,8 @@ import { detectCountry, get_env } from '../utils'
 import { UserIcon } from '../assets/icons'
 import { useUser, CartContext } from '../context'
 import { useToasts } from 'react-toast-notifications'
+import { Tunnel } from './tunnel'
+import classNames from 'classnames'
 
 export const UserInfo = props => {
    const { cart, editable = true } = props
@@ -27,6 +29,7 @@ export const UserInfo = props => {
          user.platform_customer?.phoneNumber ||
          ''
    )
+   const [openTunnel, setOpenTunnel] = useState(false)
 
    const onBlurData = type => {
       let infoToBeSend
@@ -59,9 +62,13 @@ export const UserInfo = props => {
          },
       })
    }
-   const UserInfoHeader = () => {
+   const UserInfoHeader = ({ insideTunnel = false }) => {
       return (
-         <div className="hern-user-info__header">
+         <div
+            className={classNames('hern-user-info__header', {
+               'hern-user-info__header--title': insideTunnel,
+            })}
+         >
             <UserIcon size={16} />
             <h2 className="hern-user-info__heading">User Details </h2>
          </div>
@@ -85,83 +92,121 @@ export const UserInfo = props => {
    //       </div>
    //    )
    // }
-   return (
-      <div className="hern-user-info">
-         <UserInfoHeader />
-         <div className="hern-user-info__name-field">
-            <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-first-name">
-               <label className="hern-user-info__label">First Name</label>
-               <input
-                  name="user-first-name"
-                  type="text"
-                  className="hern-user-info__input-field"
+   const UserInfoContent = ({ insideTunnel = false }) => {
+      return (
+         <div
+            className={classNames('hern-user-info', {
+               'hern-user-info__inside-tunnel': insideTunnel,
+            })}
+         >
+            {!insideTunnel && <UserInfoHeader />}
+            <div className="hern-user-info__name-field">
+               <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-first-name">
+                  <label className="hern-user-info__label">First Name</label>
+                  <input
+                     name="user-first-name"
+                     type="text"
+                     className="hern-user-info__input-field"
+                     onChange={e => {
+                        setFirstName(e.target.value)
+                     }}
+                     value={firstName}
+                     placeholder="Enter your first name"
+                     onBlur={() => {
+                        if (
+                           !(cart?.customerInfo?.customerFirstName == firstName)
+                        ) {
+                           onBlurData('firstName')
+                        }
+                     }}
+                     disabled={!editable}
+                  />
+               </fieldset>
+               <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-last-name">
+                  <label className="hern-user-info__label">Last Name</label>
+                  <input
+                     name="user-last-name"
+                     type="text"
+                     className="hern-user-info__input-field"
+                     onChange={e => {
+                        setLastName(e.target.value)
+                     }}
+                     value={lastName}
+                     placeholder="Enter your last name"
+                     onBlur={() => {
+                        if (
+                           !(cart?.customerInfo?.customerLastName == lastName)
+                        ) {
+                           onBlurData('lastName')
+                        }
+                     }}
+                     disabled={!editable}
+                  />
+               </fieldset>
+            </div>
+            <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-phone-number">
+               <label className="hern-user-info__label">Phone Number</label>
+               <PhoneInput
+                  className={`hern-user-info__phone__input hern-user-info__phone__input${
+                     !(mobileNumber && isValidPhoneNumber(mobileNumber))
+                        ? '-invalid'
+                        : '-valid'
+                  }`}
+                  initialValueFormat="national"
+                  value={mobileNumber}
                   onChange={e => {
-                     setFirstName(e.target.value)
+                     setMobileNumber(e)
                   }}
-                  value={firstName}
-                  placeholder="Enter your first name"
                   onBlur={() => {
                      if (
-                        !(cart?.customerInfo?.customerFirstName == firstName)
+                        mobileNumber &&
+                        isValidPhoneNumber(mobileNumber) &&
+                        !(cart?.customerInfo?.customerPhone == mobileNumber)
                      ) {
-                        onBlurData('firstName')
+                        onBlurData('phoneNumber')
                      }
                   }}
+                  defaultCountry={get_env('COUNTRY_CODE')}
+                  placeholder="Enter your phone number"
                   disabled={!editable}
                />
-            </fieldset>
-            <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-last-name">
-               <label className="hern-user-info__label">Last Name</label>
-               <input
-                  name="user-last-name"
-                  type="text"
-                  className="hern-user-info__input-field"
-                  onChange={e => {
-                     setLastName(e.target.value)
-                  }}
-                  value={lastName}
-                  placeholder="Enter your last name"
-                  onBlur={() => {
-                     if (!(cart?.customerInfo?.customerLastName == lastName)) {
-                        onBlurData('lastName')
-                     }
-                  }}
-                  disabled={!editable}
-               />
+               <span className="hern-user-info__phone-number-warning">
+                  {mobileNumber &&
+                     !isValidPhoneNumber(mobileNumber) &&
+                     'Invalid phone number'}
+               </span>
             </fieldset>
          </div>
-         <fieldset className="hern-user-info__fieldset hern-user-info__fieldset-phone-number">
-            <label className="hern-user-info__label">Phone Number</label>
-            <PhoneInput
-               className={`hern-user-info__phone__input hern-user-info__phone__input${
-                  !(mobileNumber && isValidPhoneNumber(mobileNumber))
-                     ? '-invalid'
-                     : '-valid'
-               }`}
-               initialValueFormat="national"
-               value={mobileNumber}
-               onChange={e => {
-                  setMobileNumber(e)
-               }}
-               onBlur={() => {
-                  if (
-                     mobileNumber &&
-                     isValidPhoneNumber(mobileNumber) &&
-                     !(cart?.customerInfo?.customerPhone == mobileNumber)
-                  ) {
-                     onBlurData('phoneNumber')
-                  }
-               }}
-               defaultCountry={get_env('COUNTRY_CODE')}
-               placeholder="Enter your phone number"
-               disabled={!editable}
-            />
-            <span className="hern-user-info__phone-number-warning">
-               {mobileNumber &&
-                  !isValidPhoneNumber(mobileNumber) &&
-                  'Invalid phone number'}
-            </span>
-         </fieldset>
-      </div>
+      )
+   }
+   return (
+      <>
+         <div className="hern-user-info--sm">
+            <div>
+               <span>
+                  <UserIcon size={16} />
+               </span>
+               <div>
+                  <span>{firstName + ' ' + lastName}</span>
+                  <span className="hern-user-info--sm__phone-no">
+                     {mobileNumber}
+                  </span>
+               </div>
+            </div>
+            <button onClick={() => setOpenTunnel(true)}>Edit</button>
+         </div>
+         <div className="hern-user-info__wrapper">
+            <UserInfoContent />
+         </div>
+
+         <Tunnel.Bottom
+            title={<UserInfoHeader insideTunnel={true} />}
+            visible={openTunnel}
+            className="hern-user-info__tunnel"
+            onClose={() => setOpenTunnel(false)}
+         >
+            <UserInfoContent insideTunnel={true} />
+         </Tunnel.Bottom>
+      </>
    )
 }
