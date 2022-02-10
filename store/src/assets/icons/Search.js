74d18c4c9a37dbe7b6960ea26c5018ab25db2dd6
rahul72925 <rahulkhatri72925@gmail.com>
2022-02-10 17:42:47 +0530

    feat: google place autocomplete in refine location

---
+++ store/src/assets/icons/Search.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const SearchIcon = ({ size = 18 }) => {
+   return (
+      <svg
+         width={size}
+         height={size}
+         viewBox="0 0 18 19"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M12.9583 13.8767C14.1479 12.5794 14.8773 10.8288 14.8773 8.90249C14.8773 4.90296 11.7327 1.66071 7.8537 1.66071C3.97466 1.66071 0.830078 4.90296 0.830078 8.90249C0.830078 12.902 3.97466 16.1443 7.8537 16.1443C9.86449 16.1443 11.6779 15.273 12.9583 13.8767ZM12.9583 13.8767L16.8301 17.6607"
+            stroke="#404040"
+            stroke-opacity="0.4"
+            stroke-width="1.5"
+            stroke-linecap="round"
+         />
+      </svg>
+   )
+}
