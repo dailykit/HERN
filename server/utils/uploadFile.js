8907ef965f07fcb2a14440b7c3f7b3e3ceae2245
Rishi Kumar <rishikumar@Rishis-MacBook-Pro.local>
2022-02-23 16:41:27 +0530

    fix: compress logic in upload util

--- server/utils/uploadFile.js
+++ server/utils/uploadFile.js
@@ -7,10 +7,9 @@ export const uploadFile = async (buffer, name, type) => {
    const s3 = new AWS.S3()
    const S3_BUCKET = await get_env('S3_BUCKET')
    let s3buffer
-
    // minify image buffer
-   const validImageTypes = ['png', 'jpg', 'jpeg', 'webp', 'tiff', 'eps', 'gif']
-   if (validImageTypes.includes(type.ext)) {
+   const validImageTypes = ['png', 'jpg', 'jpeg', 'webp', 'tiff']
+   if (validImageTypes.includes(type.ext) && buffer.length > 100000) {
       s3buffer = await minifyImage(buffer, type)
    } else {
       s3buffer = buffer
