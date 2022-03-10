a03b2edc5f8844908de5dae5b32e6a47fc2aa660
FASami <foysal.developer@gmail.com>
2022-03-10 16:22:28 +0600

    feat: login wrapper desktop view

--- store/src/utils/loginWrapper.js
+++ store/src/utils/loginWrapper.js
@@ -16,10 +16,18 @@ export const LoginWrapper = ({ ...props }) => {
          unmountOnExit
          classNames="hern-login-v1__css-transition"
       >
-         <Login
-            {...props}
-            socialLogin={authConfig.socialLoginMethods?.socialLogin?.value}
-         />
+         <div className="hern-login-v1-container">
+            <div className="hern-login-v1-container__img">
+               <img
+                  src="https://dailykit-133-test.s3.us-east-2.amazonaws.com/misc/07680-user%20login.svg"
+                  alt="Login"
+               />
+            </div>
+            <Login
+               {...props}
+               socialLogin={authConfig.socialLoginMethods?.socialLogin?.value}
+            />
+         </div>
       </CSSTransition>
    )
 }
