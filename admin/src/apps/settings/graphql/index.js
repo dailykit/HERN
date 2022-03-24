ea3425486d3222ec0a7b3f24207ab55daa847daf
nitin <inbox.nitin.goyal@gmail.com>
2022-03-24 14:12:30 +0530

    feat: query for brand and location listing in user form

--- admin/src/apps/settings/graphql/index.js
+++ admin/src/apps/settings/graphql/index.js
@@ -631,11 +631,11 @@ export const MASTER = {
       LIST_ONE: gql`
          subscription productCategory($name: String = "") {
             productCategory(name: $name) {
-            iconUrl
-            imageUrl
-            bannerImageUrl
-            name
-            metaDetails
+               iconUrl
+               imageUrl
+               bannerImageUrl
+               name
+               metaDetails
             }
          }
       `,
@@ -667,12 +667,28 @@ export const MASTER = {
          }
       `,
       UPDATE: gql`
-         mutation updateProductCategory($name: String = "", $iconUrl: String = "", $imageUrl: String = "", $bannerImageUrl: String = "", $metaDetails: jsonb = "", $updatedName: String = "") {
-            updateProductCategory(pk_columns: {name: $name}, _set: {iconUrl: $iconUrl, imageUrl: $imageUrl, bannerImageUrl: $bannerImageUrl, metaDetails: $metaDetails, name: $updatedName}) {
-            name
+         mutation updateProductCategory(
+            $name: String = ""
+            $iconUrl: String = ""
+            $imageUrl: String = ""
+            $bannerImageUrl: String = ""
+            $metaDetails: jsonb = ""
+            $updatedName: String = ""
+         ) {
+            updateProductCategory(
+               pk_columns: { name: $name }
+               _set: {
+                  iconUrl: $iconUrl
+                  imageUrl: $imageUrl
+                  bannerImageUrl: $bannerImageUrl
+                  metaDetails: $metaDetails
+                  name: $updatedName
+               }
+            ) {
+               name
             }
          }
-      `
+      `,
    },
    INGREDIENT_CATEGORY: {
       LIST: gql`
@@ -1017,3 +1033,29 @@ export const APPS = {
       }
    `,
 }
+export const SCOPE_SELECTOR = gql`
+   subscription brandId($identifier: String!) {
+      brandsAggregate(order_by: { id: asc }) {
+         aggregate {
+            count
+         }
+         nodes {
+            title
+            id
+            domain
+            isDefault
+            brand_brandSettings(
+               where: { brandSetting: { identifier: { _eq: $identifier } } }
+            ) {
+               value
+            }
+            brand_locations {
+               location {
+                  id
+                  label
+               }
+            }
+         }
+      }
+   }
+`
