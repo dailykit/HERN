99c0e92aad6e06873257e68b462255817b0bcb68
Deepak Negi <st.deepak15@gmail.com>
2022-03-29 15:18:50 +0530

    feat:sendOtp event in server/entity/events

---
+++ server/entities/events/sendOtp.js
@@ -0,0 +1,38 @@
+import { client } from '../../lib/graphql'
+export const sendOtp = async (req, res) => {
+   try {
+      const { id, phoneNumber, resendAttempts, code } = req.body.event.data.new
+      if (resendAttempts >= 3) {
+         return res.status(429).json({
+            success: false,
+            message: 'You have reached maximum resend attempts'
+         })
+      }
+
+      const { sendSMS } = await client.request(SEND_SMS, {
+         message: `Your OTP is ${code}`,
+         phone: phoneNumber
+      })
+
+      if (sendSMS) {
+         return res.status(200).json({
+            success: true,
+            message: 'OTP sent successfully'
+         })
+      }
+   } catch (error) {
+      return res.status(400).json({
+         success: false,
+         message: error.message
+      })
+   }
+}
+
+export const SEND_SMS = `
+   mutation sendSMS($message: String!, $phone: String!) {
+      sendSMS(message: $message, phone: $phone) {
+         success
+         message
+      }
+   }
+`
