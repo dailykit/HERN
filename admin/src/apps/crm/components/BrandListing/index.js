4e90a67ed8bb4ddf1c4d636fba3cf2e717ebd3d2
nitin <inbox.nitin.goyal@gmail.com>
2022-04-06 14:22:20 +0530

    feat: add brand list for customer entity in crm app

--- admin/src/apps/crm/components/BrandListing/index.js
+++ admin/src/apps/crm/components/BrandListing/index.js
@@ -1,64 +1,81 @@
-import React, { useState, useContext } from 'react'
+import React from 'react'
 import { useSubscription } from '@apollo/react-hooks'
-import { toast } from 'react-toastify'
+import {
+   Filler,
+   List,
+   ListHeader,
+   ListItem,
+   ListOptions,
+   ListSearch,
+   TunnelHeader,
+   useSingleList,
+} from '@dailykit/ui'
 import { BRAND_LISTING } from '../../graphql'
-import { logger } from '../../../../shared/utils'
-import { InlineLoader } from '../../../../shared/components'
-import BrandContext from '../../context/Brand'
-// Styled
-import { StyledList, StyledListItem, StyledHeading } from './styled'
-export default function BrandListing() {
-   const [context, setContext] = useContext(BrandContext)
-   const [brandList, setBrandList] = useState([])
-   const [viewingFor, setViewingFor] = useState('')
+import { Banner, InlineLoader } from '../../../../shared/components'
+import { TunnelContainer } from '../../../inventory/components'
+import { useTabs } from '../../../../shared/providers'
+
+const BrandListing = ({ closeTunnel }) => {
+   const [brandList, setBrandList] = React.useState([])
+   const { tab, addTab } = useTabs()
+
+   //subscription
    const { loading, error } = useSubscription(BRAND_LISTING, {
       onSubscriptionData: data => {
-         const result = data.subscriptionData.data.brands
-         setBrandList(result)
-         result.map(brand => {
-            if (brand.isDefault) {
-               setViewingFor(brand.id)
-               setContext({
-                  brandId: brand.id,
-                  brandName: brand.title,
-                  domain: brand.domain,
-               })
-            }
-         })
+         setBrandList(data.subscriptionData.data.brands)
       },
    })
+   // console.log('brand ID:::', brandList)
 
-   const brandHandler = brand => {
-      toast.info(`Showing information for "${brand.title}" brand`)
-      setViewingFor(brand.id)
-      setContext({
-         brandId: brand.id,
-         brandName: brand.title,
-         domain: brand.domain,
-      })
-   }
+   //ssl1 declaration
+   const [list, current, selectOption] = useSingleList(brandList)
+   const [search, setSearch] = React.useState('')
 
-   if (error) {
-      toast.error('Something went wrong!!')
-      logger(error)
-   }
-   if (loading) return <InlineLoader />
    return (
-      <div>
-         <StyledHeading>Viewing For</StyledHeading>
-         <StyledList>
-            {brandList.map(brand => {
-               return (
-                  <StyledListItem
-                     key={brand.id}
-                     default={brand.id === viewingFor}
-                     onClick={() => brandHandler(brand)}
-                  >
-                     {brand.title}
-                  </StyledListItem>
-               )
-            })}
-         </StyledList>
-      </div>
+      <>
+         <TunnelHeader
+            title="Select Brand for Customer Details"
+            close={() => closeTunnel(1)}
+            nextAction="Done"
+         />
+         {loading ? (
+            <InlineLoader />
+         ) : (
+            <TunnelContainer>
+               <Banner id="brand-tunnel-list-top" />
+               {list.length ? (
+                  <List>
+                     <ListSearch
+                        onChange={value => setSearch(value)}
+                        placeholder="type what you’re looking for..."
+                     />
+                     <ListHeader type="SSL1" label="Brand" />
+                     <ListOptions>
+                        {list
+                           .filter(option =>
+                              option.title.toLowerCase().includes(search)
+                           )
+                           .map(option => (
+                              <ListItem
+                                 type="SSL1"
+                                 key={option.id}
+                                 title={option.title}
+                                 isActive={option.id === current.id}
+                                 onClick={() =>
+                                    addTab('Customers', '/crm/customers')
+                                 }
+                              />
+                           ))}
+                     </ListOptions>
+                  </List>
+               ) : (
+                  <Filler message="Sorry!, No Brand is available" />
+               )}
+               <Banner id="brand-tunnel-list-bottom" />
+            </TunnelContainer>
+         )}
+      </>
    )
 }
+
+export default BrandListing
