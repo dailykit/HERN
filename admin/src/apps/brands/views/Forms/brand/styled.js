659317eaf4f345139e02296e2756c4d9683e03a6
nitin <inbox.nitin.goyal@gmail.com>
2022-04-08 14:19:13 +0530

    style: brand coupon list

--- admin/src/apps/brands/views/Forms/brand/styled.js
+++ admin/src/apps/brands/views/Forms/brand/styled.js
@@ -18,3 +18,86 @@ export const Label = styled.label`
    letter-spacing: 0.6px;
    text-transform: uppercase;
 `
+// export const CouponCard = styled.div`
+//    display: flex;
+//    align-items: center;
+//    width: 100%;
+// `
+
+export const CouponCard = styled.div`
+   background: #ffffff;
+   box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.08);
+   height: 66px;
+   margin-bottom: 16px;
+   grid-template-columns: 8% auto 12% 8%;
+   display: grid;
+   clip-path: polygon(
+      10% 20%,
+      12% 0,
+      100% 0,
+      100% 100%,
+      12% 100%,
+      10% 80%,
+      8% 100%,
+      0 100%,
+      0 0,
+      8% 0
+   );
+   background: #f9f9f9;
+   border-radius: 10px;
+   align-items: center;
+`
+export const StyledIcon = styled.div`
+   display: flex;
+   justify-content: center;
+`
+export const StyledText = styled.div`
+   left: 7%;
+   position: relative;
+   font-family: 'Roboto';
+   font-style: normal;
+   font-weight: 500;
+   font-size: 18px;
+   line-height: 20px;
+
+   display: flex;
+   align-items: center;
+   letter-spacing: 0.44px;
+
+   color: #202020;
+`
+
+// export const CardContext = styled.div`
+//    display: grid;
+//    position: absolute;
+//    align-items: center;
+//    width: 95%;
+//    grid-template-columns: 80px auto 1rem 10px;
+// `
+// export const Card1 = styled.div`
+//    left: 3.5%;
+//    position: relative;
+// `
+// export const Card2 = styled.div`
+//    left: 9.5%;
+//    position: relative;
+//    font-family: 'Roboto';
+//    font-style: normal;
+//    font-weight: 500;
+//    font-size: 20px;
+//    line-height: 24px;
+
+//    display: flex;
+//    align-items: center;
+//    letter-spacing: 0.44px;
+
+//    color: #202020;
+// `
+// export const Card3 = styled.div`
+//    left: 30%;
+//    position: relative;
+// `
+// export const Card4 = styled.div`
+//    left: 50%;
+//    position: relative;
+// `
