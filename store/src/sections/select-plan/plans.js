a12aa766d4390cb93a25aa43fb0e87d606a4ed6d
Andrew Ingombe <ingombe.ani@gmail.com>
2022-06-09 10:57:12 +0200

    fix: Fix cover image warpper on card view of select plan.

--- store/src/sections/select-plan/plans.js
+++ store/src/sections/select-plan/plans.js
@@ -18,7 +18,7 @@ export const Plans = ({ config }) => {
    const [isLoading, setIsLoading] = React.useState(true)
 
    // Plan view Config
-   const planViewConfig = config?.display?.planView?.value?.value ?? 'card'
+   const planViewConfig = 'card'
 
    //Config
    const headingConfig = {
