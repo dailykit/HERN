da918c17a35cf94a45cf8e1263c7afefbbb37f0b
Andrew Ingombe <ingombe.ani@gmail.com>
2022-06-09 12:48:21 +0200

    fix: Extract the plate and card cover illustration in separate files for resuability.

--- store/src/sections/select-plan/plans.js
+++ store/src/sections/select-plan/plans.js
@@ -9,6 +9,7 @@ import { useConfig } from '../../lib'
 import { useUser } from '../../context'
 import { SkeletonPlan } from './skeletons'
 import { HelperBar } from '../../components'
+import { PlateIllustration } from '../../assets/icons/PlateIllustration'
 
 export const Plans = ({ config }) => {
    const { user } = useUser()
@@ -271,7 +272,9 @@ const PlansDetails = ({ selectedPlan, list, config, planViewConfig }) => {
                   {/* TODO-
                   - Include plateIllustration
                 */}
-
+                  <span>
+                     <PlateIllustration />
+                  </span>
                   {selectedPlan.title}
                </p>
                <Plan
