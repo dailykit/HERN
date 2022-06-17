d726b6a3e428ddd3bb7f69fd82d5ce6ae1ef1ccf
FASami <foysal.developer@gmail.com>
2022-06-17 13:50:27 +0600

    feat: added variant for ArrowLeftWithBG icon

--- store/src/assets/icons/ArrowLeftWithBG.js
+++ store/src/assets/icons/ArrowLeftWithBG.js
@@ -5,8 +5,29 @@ export const ArrowLeftIconBG = props => {
       bgColor = '#0F6BB1',
       size = 56,
       arrowColor = 'white',
+      variant = 'lg',
       ...rest
    } = props
+   if (variant === 'sm') {
+      return (
+         <svg
+            width="42"
+            height="42"
+            viewBox="0 0 42 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+         >
+            <rect width="42" height="42" rx="21" fill="#7124B4" />
+            <path
+               d="M31.5 21.4113H10.5M10.5 21.4113L18.7889 14.8749M10.5 21.4113L18.7889 27.1249"
+               stroke="white"
+               stroke-width="3"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+            />
+         </svg>
+      )
+   }
    return (
       <svg
          width={size}
