91dab7598957ac7a4695c25c2325bf1262629bc3
rahul72925 <rahulkhatri72925@gmail.com>
2022-07-14 16:26:39 +0530

    fix: error on link brandCustomer with device

--- server/entities/auth/controllers.js
+++ server/entities/auth/controllers.js
@@ -12,106 +12,119 @@ import get_env from '../../../get_env'
 
 export const authHandler = async (req, res) => {
    const { authType } = JSON.parse(req.query.authDetails)
-   console.log('authType', JSON.parse(req.query.authDetails))
+   // console.log('authType', JSON.parse(req.query.authDetails))
    if (authType === 'otp') {
-      const {
-         phoneNumber,
-         otp,
-         email = null,
-         notificationToken,
-         brandId
-      } = JSON.parse(req.query.authDetails)
-      if (!Boolean(phoneNumber) || !Boolean(otp)) {
-         res.status(404).json({
-            message: `Provide${!Boolean(phoneNumber) ? ' phoneNumber' : ''}${
-               !Boolean(phoneNumber) && !Boolean(otp) ? ' and' : ''
-            }${!Boolean(otp) ? ' otp' : ''}`
+      try {
+         const {
+            phoneNumber,
+            otp,
+            email = null,
+            notificationToken,
+            brandId
+         } = JSON.parse(req.query.authDetails)
+         if (!Boolean(phoneNumber) || !Boolean(otp)) {
+            res.status(404).json({
+               message: `Provide${!Boolean(phoneNumber) ? ' phoneNumber' : ''}${
+                  !Boolean(phoneNumber) && !Boolean(otp) ? ' and' : ''
+               }${!Boolean(otp) ? ' otp' : ''}`
+            })
+         }
+         const { otps = [] } = await client.request(OTPS, {
+            where: { phoneNumber: { _eq: phoneNumber } }
          })
-      }
-      const { otps = [] } = await client.request(OTPS, {
-         where: { phoneNumber: { _eq: phoneNumber } }
-      })
-      if (otps.length > 0) {
-         const [firstOtp] = otps
-         if (Number(otp) === firstOtp.code) {
-            const { platform_customer = [] } = await client.request(
-               PLATFORM_CUSTOMER,
-               {
-                  where: { phoneNumber: { _eq: phoneNumber } }
-               }
-            )
-            let customerInfo
-            if (platform_customer.length > 0) {
-               const [customer] = platform_customer
-               customerInfo = customer
-            } else {
-               const { insertCustomer = {} } = await client.request(
-                  INSERT_CUSTOMER,
+         if (otps.length > 0) {
+            const [firstOtp] = otps
+            if (Number(otp) === firstOtp.code) {
+               const { platform_customer = [] } = await client.request(
+                  PLATFORM_CUSTOMER,
                   {
-                     object: {
-                        ...(email && {
-                           email: email
-                        }),
-                        phoneNumber: phoneNumber
-                     }
+                     where: { phoneNumber: { _eq: phoneNumber } }
                   }
                )
-               await client.request(INSERT_BRAND_CUSTOMER, {
-                  object: {
-                     sourceBrandId: brandId,
-                     brandCustomers: {
-                        data: {
-                           brandId: brandId
+               let customerInfo
+               if (platform_customer.length > 0) {
+                  const [customer] = platform_customer
+                  customerInfo = customer
+               } else {
+                  const { insertCustomer = {} } = await client.request(
+                     INSERT_CUSTOMER,
+                     {
+                        object: {
+                           ...(email && {
+                              email: email
+                           }),
+                           phoneNumber: phoneNumber
+                        }
+                     }
+                  )
+                  await client.request(INSERT_BRAND_CUSTOMER, {
+                     object: {
+                        sourceBrandId: brandId,
+                        brandCustomers: {
+                           data: {
+                              brandId: brandId
+                           }
+                        },
+                        email: email,
+                        keycloakId: insertCustomer.id
+                     }
+                  })
+                  customerInfo = insertCustomer
+               }
+
+               // get brand customer and mobile device id
+               const { brandCustomers = [], deviceHub_mobileDevice = [] } =
+                  await client.request(BRAND_CUSTOMER_AND_DEVICE_ID, {
+                     where: {
+                        keycloakId: {
+                           _eq: customerInfo.id
+                        },
+                        brandId: {
+                           _eq: brandId
                         }
                      },
-                     email: email,
-                     keycloakId: insertCustomer.id
-                  }
-               })
-               customerInfo = insertCustomer
-            }
-
-            // get brand customer and mobile device id
-            const { brandCustomers = [], deviceHub_mobileDevice = [] } =
-               await client.request(BRAND_CUSTOMER_AND_DEVICE_ID, {
-                  where: {
-                     keycloakId: {
-                        _eq: customerInfo.id
-                     },
-                     brandId: {
-                        _eq: brandId
+                     where1: {
+                        notificationToken: {
+                           _eq: notificationToken
+                        }
                      }
-                  },
-                  where1: {
-                     notificationToken: {
-                        _eq: notificationToken
+                  })
+               // insert new entry for brandCustomerId with mobileDeviceId
+               if (
+                  brandCustomers.length > 0 &&
+                  deviceHub_mobileDevice.length > 0
+               ) {
+                  await client.request(INSERT_BRAND_CUSTOMER_ID_DEVICE_ID, {
+                     object: {
+                        brandCustomerId: brandCustomers[0].id,
+                        mobileDeviceId: deviceHub_mobileDevice[0].id
                      }
-                  }
-               })
-            // insert new entry for brandCustomerId with mobileDeviceId
-            await client.request(INSERT_BRAND_CUSTOMER_ID_DEVICE_ID, {
-               object: {
-                  brandCustomerId: brandCustomers[0].id,
-                  mobileDeviceId: deviceHub_mobileDevice[0].id
+                  })
                }
-            })
 
-            const secretKey = await get_env('HASURA_GRAPHQL_ADMIN_SECRET')
+               const secretKey = await get_env('HASURA_GRAPHQL_ADMIN_SECRET')
 
-            const token = jwt.sign({ ...customerInfo }, secretKey, {
-               expiresIn: '365d'
-            })
-            return res.status(200).json({
-               token
+               const token = jwt.sign({ ...customerInfo }, secretKey, {
+                  expiresIn: '365d'
+               })
+               return res.status(200).json({
+                  token
+               })
+            }
+            return res.status(400).json({
+               message: 'Incorrect OTP'
             })
          }
          return res.status(400).json({
-            message: 'Incorrect OTP'
+            message: 'OTP not available'
+         })
+      } catch (err) {
+         console.log('err', err)
+         return res.status(400).json({
+            message: err,
+            success: false
          })
       }
-      return res.status(400).json({
-         message: 'OTP not available'
-      })
    }
    return res.status(400).json({
       message: 'There should be a authType.'
